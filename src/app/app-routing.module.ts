a124be40737cc3287b866a5da93553f508e703b3
Jefferson SA <jeff_zb@msn.com>
2022-04-29 16:41:11 -0300

    criado componente pagina nao-encontrado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NaoEncontradoComponent } from './errors/nao-encontrado/nao-encontrado.component';
 
 import { FotoListComponent } from './fotos/foto-list/foto-list.component';
 
 const routes: Routes = [
-  { path: 'listarfotos', component: FotoListComponent }
+  { path: 'listarfotos', component: FotoListComponent },
+  { path: '**', component: NaoEncontradoComponent }
 ];
 
 @NgModule({
