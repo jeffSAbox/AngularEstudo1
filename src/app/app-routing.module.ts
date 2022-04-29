00dbdd7a24a370248af70923d4a586ff3d750c01
Jefferson SA <jeff_zb@msn.com>
2022-04-29 16:28:59 -0300

    criado router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FotoListComponent } from './fotos/foto-list/foto-list.component';
+
+const routes: Routes = [
+  { path: 'listarfotos', component: FotoListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
